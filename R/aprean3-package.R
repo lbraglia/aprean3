27a3f9b821b67ed8e0ec7b3e1dfa0db95f4c8e8f
lbraglia <lbraglia@gmail.com>
2014-12-26 09:52:48 +0100

    description update

--- R/aprean3-package.R
+++ R/aprean3-package.R
@@ -3,9 +3,8 @@
 #' This package is a unofficial companion to the textbook "Applied
 #' Regression Analysis" by N.R. Draper and H. Smith (3rd ed.).
 #'
-#' It includes almost all the datasets used in the book, both for easy
-#' reproducibility (assuming a didactic point of view) and algorithms
-#' benchmarking purposes.
+#' It includes almost all the published datasets, both for easy
+#' reproducibility and algorithms benchmarking purposes.
 #'
 #' Dataset names are prefixed by 'ds' and lowercased; furthermore '-' and
 #' '.' are replaced by '_'. Finally variables have lower-case name. 
